07dc3fccc1697299fc7e3332d54753f4c657ed0b
pwa-test123 <70209940+pwa-test123@users.noreply.github.com>
2020-08-25 16:59:51 +0400

    Add files via upload
    test

---
+++ precache-manifest.d04abb873b1f25dba7e2be8ed4cd1f24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54122f8e8579814454cc814ff56c2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3256cb8f42d2aac35d1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dd2cd993ef0bb20739a7",
+    "url": "/static/js/2.8c5f86fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c5f86fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3256cb8f42d2aac35d1",
+    "url": "/static/js/main.7fd9038d.chunk.js"
+  },
+  {
+    "revision": "8687a13f8b27fde15bf9",
+    "url": "/static/js/runtime-main.cfe7aea2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
